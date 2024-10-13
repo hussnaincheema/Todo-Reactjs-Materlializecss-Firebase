f6811c65a53dbb3e79ee7bf63f9e240a7eb5387c
Hussnain Cheema <76845698+hussnaincheema@users.noreply.github.com>
2024-10-13 05:12:55 +0400

    Final Commit

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAzi03gK8P1plIuMRlmZldm5rZWUk0IQ7w",
-  authDomain: "todo-12ed2.firebaseapp.com",
-  projectId: "todo-12ed2",
-  storageBucket: "todo-12ed2.appspot.com",
-  messagingSenderId: "794738441702",
-  appId: "1:794738441702:web:baec63d714b466be70ea27",
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
 };
 
 const app = initializeApp(firebaseConfig);
